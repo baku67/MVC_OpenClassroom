b72d5ead8ef08d05b01be35eb61b6430a29d0ed3
Basile(TUF) <basile08@hotmail.fr>
2022-12-11 16:24:45 +0100

    Ajout layout

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
@@ -29,4 +29,37 @@
         } // The end of the posts loop.
         ?>
     </body>
-</html>
+</html> -->
+
+
+
+
+
+<?php $title = "Le blog de l'AVBN"; ?>
+
+<?php ob_start(); ?>
+<h1>Le super blog de l'AVBN</h1>
+<p>Derniers billets du blog :</p>
+
+<?php
+foreach ($posts as $post) {
+?>
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($post['title']); ?>
+            <em>le <?= $post['french_creation_date']; ?></em>
+        </h3>
+        <p>
+            <?= nl2br(htmlspecialchars($post['content']));
+            ?>
+            <br />
+            <em><a href="post.php?id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em>
+        </p>
+    </div>
+<?php
+} // The end of the posts loop.
+?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('layout.php') ?>
+
