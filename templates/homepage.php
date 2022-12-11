320f74b3d7d9d82664796ff9b02341db21b61c84
Basile(TUF) <basile08@hotmail.fr>
2022-12-11 18:32:42 +0100

    Ajout routeur

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,40 +1,3 @@
-<!-- <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Le blog de l'AVBN</title>
-        <link href="style.css" rel="stylesheet" />
-    </head>
-
-    <body>
-        <h1>Le super blog de l'AVBN !</h1>
-        <p>Derniers billets du blog :</p>
-
-        <?php
-        foreach ($posts as $post) {
-        ?>
-            <div class="news">
-                <h3>
-                    <?= htmlspecialchars($post['title']); ?>
-                    <em>le <?= $post['french_creation_date']; ?></em>
-                </h3>
-                <p>
-                    <?= nl2br(htmlspecialchars($post['content']));
-                    ?>
-                    <br />
-                    <em><a href="post.php?id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em>
-                </p>
-            </div>
-        <?php
-        } // The end of the posts loop.
-        ?>
-    </body>
-</html> -->
-
-
-
-
-
 <?php $title = "Le blog de l'AVBN"; ?>
 
 <?php ob_start(); ?>
@@ -53,7 +16,8 @@ foreach ($posts as $post) {
             <?= nl2br(htmlspecialchars($post['content']));
             ?>
             <br />
-            <em><a href="post.php?id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em>
+            <!-- <em><a href="post.php?id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em> -->
+            <em><a href="index.php?action=post&id=<?= urlencode($post['identifier'])?>">Commentaires</a></em>
         </p>
     </div>
 <?php
